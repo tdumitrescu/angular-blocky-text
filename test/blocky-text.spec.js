fd1068dedc8cf41f0267b2f13eb536e092d1d213
t ddddddd <miscmisc@cmme.org>
2013-12-16 20:00:30 -0800

    add tests for cases with divider char

--- test/blocky-text.spec.js
+++ test/blocky-text.spec.js
@@ -2,24 +2,58 @@ describe('blocky-text directive', function() {
 
   beforeEach(module('tdumitrescu.blocky-text'));
 
-  beforeEach(inject(function($compile, $rootScope) {
-    myStr = "halloworld";
+  function prepareEl(s, elHtml) {
+    inject(function($compile, $rootScope) {
+      myStr   = s;
+      myChars = myStr.split('');
 
-    scope = $rootScope;
-    scope.myExpr = myStr;
-    el = $compile("<td-blocky-text text='myExpr'/>")(scope);
-    scope.$digest();
+      scope = $rootScope;
+      scope.myExpr = myStr;
+      el = $compile(elHtml)(scope);
+      scope.$digest();
 
-    spans = el.find('span');
-  }));
+      spans = el.find('span');
+    });
+  }
 
-  it('separates the text into span elements', function() {
-    expect(spans.length).toBe(myStr.length);
+  describe('with no divider specified', function() {
+    beforeEach(function() {
+      prepareEl("halloworld", "<td-blocky-text text='myExpr'/>");
+    });
+
+    it('separates the text into span elements', function() {
+      expect(spans.length).toBe(myStr.length);
+    });
+
+    it('inserts the correct characters into the elements', function() {
+      myChars.forEach(function(span, i) {
+        expect(spans.eq(i).text().replace(/ /g, '')).toBe(myStr[i]);
+      });
+    });
+
+    it('applies the td-blocky-text-char class to all spans', function() {
+      myChars.forEach(function(span, i) {
+        expect(spans.eq(i).hasClass('td-blocky-text-char')).toBe(true);
+      });
+    });
   });
 
-  it('inserts the correct characters into the elements', function() {
-    myStr.split('').forEach(function(span, i) {
-      expect(spans.eq(i).text().replace(/ /g, '')).toBe(myStr[i]);
+  describe('with a divider char', function() {
+    beforeEach(function() {
+      prepareEl("hallo/world", "<td-blocky-text text='myExpr' divider='/'/>");
+    });
+
+    it('separates the text into span elements including the divider', function() {
+      expect(spans.length).toBe(myStr.length);
+    });
+
+    it('adds the divider class to the correct spans', function() {
+        expect(spans.eq(5).hasClass('divider')).toBe(true);
+    });
+
+    it('does not add the divider class to the other spans', function() {
+        expect(spans.eq(0).hasClass('divider')).toBe(false);
+        expect(spans.eq(6).hasClass('divider')).toBe(false);
     });
   });
 });
